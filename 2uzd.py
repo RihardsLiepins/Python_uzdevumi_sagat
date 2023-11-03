7c58145858154e194b4b415fde79ba684c643fa3
RihardsLiepins <rihards.liepins@grobinasskola.lv>
2023-11-03 10:45:47 +0200

    commit1

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,27 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
+1)RSS?
+2) plūsmu no google.lv
 
+
+
+pip install feedparser
+
+...pip upgrade...
 """
+import feedparser
+
+# URL uz RSS plūmu
+
+rss_url='https://ltv.lsm.lv/rss?c=1'
+
+# iegūstam RSS plūsmas datus un veicam analīzi...
+kkk=feedparser.parse(rss_url)
+
+# informēšana...
+for entry in kkk.entries:
+    print("Virsraksts:", entry.title)
+    print("Saite:", entry.link)
+    print("Publicēšanas datums:", entry.published)
+    print("\n")
+
